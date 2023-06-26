170cb81cf15e228fbfb1c09e5c6dd584122a1cf6
Rafaeldfb <dfbrafa@hotmail.com>
2023-06-26 15:35:21 -0300

    [Layout] Primeira versão do layout principal.

--- pop-up-test/src/App.vue
+++ pop-up-test/src/App.vue
@@ -1,45 +1,67 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import Button from './components/atoms/Button.vue';
+// estado para qual modal mostrar
+
+const registeredModals = ['none', 'modal-1', 'modal-2'];
+
+const activeModal = ref({activeModal: registeredModals[0]});
+
+function modalHadler(target) {
+  alert('clicked')
+}
+
 </script>
 
 <template>
-  <!-- <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" /> -->
-  
   <main>
-    <h1>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</h1>
+    <h1 class="main__header-h1">Mussum Ipsum, cacilds vidis litro abertis</h1>
 
     <section>
-      Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Atirei o pau no gatis, per gatis num morreus. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Si num tem leite então bota uma pinga aí cumpadi!
-      Diuretics paradis num copo é motivis de denguis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Casamentiss faiz malandris se pirulitá.
-      A ordem dos tratores não altera o pão duris. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Per aumento de cachacis, eu reclamis. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.
-    </section>
+      <p>
+        Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. 
+        Aenean sit amet nisi. Atirei o pau no gatis, per gatis num morreus. Copo furadis é disculpa de bebadis, 
+        arcu quam euismod magna. Si num tem leite então bota uma pinga aí cumpadi!
+        Diuretics paradis num copo é motivis de denguis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis 
+        que eu levo! Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Casamentiss faiz malandris se 
+        pirulitá. A ordem dos tratores não altera o pão duris. Nullam volutpat risus nec leo commodo, ut interdum diam 
+        laoreet. Sed non consequat odio. Per aumento de cachacis, eu reclamis. Nec orci ornare consequat. 
+        Praesent lacinia ultrices consectetur. Sed non ipsum felis.
+      </p>
+      
+      <div id="buttonsContainer">
+        <Button text="Modal 1" @click="modalHadler(registeredModals[1])" ></Button>
 
-    <aside>
-      Mussum Ipsum, cacilds vidis litro abertis. Per aumento de cachacis, eu reclamis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Suco de cevadiss deixa as pessoas mais interessantis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.
-      Mé faiz elementum girarzis, nisi eros vermeio. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum.
-    </aside>
+        <Button text="modal 2" @click="modalHadler(registeredModals[2])" ></Button>
+      </div>
+    </section>
   </main>
 </template>
 
 <style scoped>
-/* .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+h1.main__header-h1 {
+  margin-top: 2rem;
+  margin-bottom: 3rem;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 5em;
+  line-height: 0.9;
+  text-align: left;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+section p {
+  text-align: start;
+  word-break: break-word;
+  word-spacing: 0.3rem;
+  line-height: 1.6;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-} */
+
+#buttonsContainer {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+#buttonsContainer > div {
+  margin-right: 2rem;
+}
 </style>
--- pop-up-test/src/components/atoms/Button.vue
+++ pop-up-test/src/components/atoms/Button.vue
@@ -4,9 +4,21 @@
 
 <template>
   <div class="button__wrapper">
-
+    <button>{{text}}</button>
   </div>
 </template>
 
 <style scoped>
+  .button__wrapper button {
+    padding: 0.5em 1.5em;
+    font-size: 16px;
+    font-weight: 600;
+    color: hsl(hue, saturation, lightness);
+    background-color: hsla(hue, saturation, lightness, alpha);
+    border-style: solid;
+    border-width: 1px;
+    border-color: hsla(hue, saturation, lightness, alpha);
+    border-radius: 1em;
+  }
+
 </style>
